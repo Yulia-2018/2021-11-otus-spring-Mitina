8af1afe0c5c3a342eb832fc28137680aba6c7ffc
MitinaUV <yulmit@mail.ru>
2021-12-04 19:23:39 +0700

    Homework 02 Add AppService, fix Main

--- homework-02/src/main/java/ru/otus/homework/Main.java
+++ homework-02/src/main/java/ru/otus/homework/Main.java
@@ -3,13 +3,7 @@ package ru.otus.homework;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
-import ru.otus.homework.domain.Question;
-import ru.otus.homework.service.QuestionService;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.List;
+import ru.otus.homework.service.AppService;
 
 @PropertySource("classpath:application.properties")
 @ComponentScan
@@ -17,30 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
-        QuestionService questionService = context.getBean(QuestionService.class);
-        String pathFileAnswers = context.getBean(String.class);
-
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
-            System.out.print("Enter your last name: ");
-            String lastName = reader.readLine().trim();
-            System.out.print("Enter your name: ");
-            String name = reader.readLine().trim();
-
-            List<String> answerList = FileUtil.getList(pathFileAnswers, (line) -> line);
-            int numberRightAnswers = 0;
-            List<Question> questionList = questionService.getAll();
-            int numberQuestions = questionList.size();
-            for (int i = 0; i < numberQuestions; i++) {
-                Question question = questionList.get(i);
-                System.out.println(question);
-                String answer = reader.readLine().trim().toLowerCase();
-                if (answer.equals(answerList.get(i).toLowerCase())) {
-                    numberRightAnswers++;
-                }
-            }
-            System.out.println(name + " " + lastName + ", the result of your testing: " + numberRightAnswers + " of " + numberQuestions + " questions");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        AppService appService = context.getBean(AppService.class);
+        appService.testStudent();
     }
 }
