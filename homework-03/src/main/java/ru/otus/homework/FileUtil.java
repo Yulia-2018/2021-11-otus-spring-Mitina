5d83f83bb6887bd5177060b09a915b888d5569eb
VESTADOM\MitinaUV <yulmit@mail.ru>
2021-12-08 16:46:14 +0700

    Change encoding to UTF-8

--- homework-03/src/main/java/ru/otus/homework/FileUtil.java
+++ homework-03/src/main/java/ru/otus/homework/FileUtil.java
@@ -1,9 +1,8 @@
 package ru.otus.homework;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
@@ -15,7 +14,7 @@ public class FileUtil {
 
     public static <T> List<T> getList(String file, Function<String, T> function) {
         List<T> list = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "Cp1251"))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 list.add(function.apply(line));
