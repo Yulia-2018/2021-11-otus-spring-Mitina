1285bb507e4d7c63759ba5d1646f7a1385dfada3
MitinaUV <yulmit@mail.ru>
2021-12-10 15:56:09 +0700

    Homework 04 Add command for Spring Shell

--- homework-04/src/main/java/ru/otus/homework/shell/ApplicationCommands.java
+++ homework-04/src/main/java/ru/otus/homework/shell/ApplicationCommands.java
@@ -1,8 +1,11 @@
 package ru.otus.homework.shell;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.shell.Availability;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellMethodAvailability;
+import org.springframework.shell.standard.ShellOption;
 import ru.otus.homework.service.AppService;
 
 @ShellComponent
@@ -10,13 +13,27 @@ public class ApplicationCommands {
 
     private final AppService service;
 
+    private String consentToTesting;
+
     @Autowired
     public ApplicationCommands(AppService service) {
         this.service = service;
+        this.consentToTesting = "-";
     }
 
-    @ShellMethod(value = "Test Student", key = {"t", "test", "testStudent"})
+    @ShellMethod(value = "Testing for students", key = {"t", "test", "testStudent"})
+    @ShellMethodAvailability(value = "isTestCommandAvailable")
     private void testStudent() {
         service.testStudent();
+    }
+
+    @ShellMethod(value = "Consent to testing", key = {"c", "consent"})
+    public String consent(@ShellOption(defaultValue = "-") String consentToTesting) {
+        this.consentToTesting = consentToTesting;
+        return consentToTesting.equals("+") ? "Вы подтвердили согласие на тестирование" : "Вы не подтвердили согласие на тестирование";
+    }
+
+    private Availability isTestCommandAvailable() {
+        return consentToTesting.equals("+") ? Availability.available() : Availability.unavailable("Сначала подтвердите согласие на тестирование");
     }
 }
