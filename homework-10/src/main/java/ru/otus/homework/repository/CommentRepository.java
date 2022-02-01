813c8170c79c01aeb0c57e76bf769b30c30ddc4a
MitinaUV <yulmit@mail.ru>
2022-02-01 18:27:15 +0700

    Delete homework-10

--- homework-10/src/main/java/ru/otus/homework/repository/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.homework.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.otus.homework.domain.Comment;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-}
