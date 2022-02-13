f401e41086be48efb7579248d3afd246a804196f
MitinaUV <yulmit@mail.ru>
2022-02-13 07:46:48 +0700

    Homework 11 Delete @DBRef for author and genre in Book

--- homework-11/src/main/java/ru/otus/homework/domain/Book.java
+++ homework-11/src/main/java/ru/otus/homework/domain/Book.java
@@ -2,7 +2,6 @@ package ru.otus.homework.domain;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
@@ -18,10 +17,8 @@ public class Book {
 
     private String title;
 
-    @DBRef
     private Author author;
 
-    @DBRef
     private Genre genre;
 
     private List<Comment> comments = new ArrayList<>();
