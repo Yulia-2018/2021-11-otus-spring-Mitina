cfa2e540ebb5b670118c6f203caa2957e0b6ef84
MitinaUV <yulmit@mail.ru>
2022-02-15 11:58:44 +0700

    Homework 11 Fix CommentController

--- homework-11/src/main/java/ru/otus/homework/controller/CommentController.java
+++ homework-11/src/main/java/ru/otus/homework/controller/CommentController.java
@@ -4,13 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import ru.otus.homework.domain.Book;
 import ru.otus.homework.domain.Comment;
 import ru.otus.homework.exception.NotFoundException;
 import ru.otus.homework.repository.BookRepository;
-
-import java.util.List;
 
 @Controller
 public class CommentController {
@@ -23,9 +22,10 @@ public class CommentController {
 
     @GetMapping("/{bookId}/comments")
     public String listPage(@PathVariable("bookId") String bookId, Model model) {
-        Mono<List<Comment>> comments = bookRepository.findById(bookId)
+        Flux<Comment> comments = bookRepository.findById(bookId)
                 .switchIfEmpty(Mono.error(new NotFoundException("Book " + bookId + " not exist")))
-                .map(Book::getComments);
+                .map(Book::getComments)
+                .flatMapMany(Flux::fromIterable);
         model.addAttribute("bookId", bookId);
         model.addAttribute("comments", comments);
         return "comments";
