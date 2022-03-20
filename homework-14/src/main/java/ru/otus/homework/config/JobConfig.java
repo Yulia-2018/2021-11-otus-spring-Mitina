4d4ca0eee6339274fb429585059c6d12f24c8b98
MitinaUV <yulmit@mail.ru>
2022-03-20 22:18:01 +0700

    Homework 14 1. Add Logger in JobConfig 2. Add listeners for steps and job

--- homework-14/src/main/java/ru/otus/homework/config/JobConfig.java
+++ homework-14/src/main/java/ru/otus/homework/config/JobConfig.java
@@ -1,10 +1,12 @@
 package ru.otus.homework.config;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.data.MongoItemReader;
 import org.springframework.batch.item.data.builder.MongoItemReaderBuilder;
@@ -25,10 +27,14 @@ import ru.otus.homework.repository.relational.R_GenreRepository;
 
 import javax.persistence.EntityManagerFactory;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Optional;
 
 @Configuration
 public class JobConfig {
+
+    private static final int CHUNK_SIZE = 5;
+    private final Logger logger = LoggerFactory.getLogger("Batch");
 
     @Autowired
     private JobBuilderFactory jobBuilderFactory;
@@ -69,10 +75,74 @@ public class JobConfig {
     public Step stepForAuthors(MongoItemReader<Author> readerAuthors, JpaItemWriter<R_Author> writerAuthors,
                                ItemProcessor<Author, R_Author> itemProcessorAuthors) {
         return stepBuilderFactory.get("stepForAuthors")
-                .<Author, R_Author>chunk(5)
+                .<Author, R_Author>chunk(CHUNK_SIZE)
                 .reader(readerAuthors)
                 .processor(itemProcessorAuthors)
                 .writer(writerAuthors)
+                .listener(new ItemReadListener<>() {
+                    @Override
+                    public void beforeRead() {
+                        logger.info("Начало чтения");
+                    }
+
+                    @Override
+                    public void afterRead(Author author) {
+                        logger.info("Конец чтения");
+                    }
+
+                    @Override
+                    public void onReadError(Exception e) {
+                        logger.info("Ошибка чтения");
+                    }
+                })
+                .listener(new ItemWriteListener<>() {
+                    @Override
+                    public void beforeWrite(List list) {
+                        logger.info("Начало записи");
+                    }
+
+                    @Override
+                    public void afterWrite(List list) {
+                        logger.info("Конец записи");
+                    }
+
+                    @Override
+                    public void onWriteError(Exception e, List list) {
+                        logger.info("Ошибка записи");
+                    }
+                })
+                .listener(new ItemProcessListener<>() {
+                    @Override
+                    public void beforeProcess(Author author) {
+                        logger.info("Начало обработки");
+                    }
+
+                    @Override
+                    public void afterProcess(Author author, R_Author r_author) {
+                        logger.info("Конец обработки");
+                    }
+
+                    @Override
+                    public void onProcessError(Author author, Exception e) {
+                        logger.info("Ошибка обработки");
+                    }
+                })
+                .listener(new ChunkListener() {
+                    @Override
+                    public void beforeChunk(ChunkContext context) {
+                        logger.info("Начало пачки");
+                    }
+
+                    @Override
+                    public void afterChunk(ChunkContext context) {
+                        logger.info("Конец пачки");
+                    }
+
+                    @Override
+                    public void afterChunkError(ChunkContext context) {
+                        logger.info("Ошибка пачки");
+                    }
+                })
                 .build();
     }
 
@@ -103,10 +173,74 @@ public class JobConfig {
     public Step stepForGenres(MongoItemReader<Genre> readerGenres, JpaItemWriter<R_Genre> writerGenres,
                               ItemProcessor<Genre, R_Genre> itemProcessorGenres) {
         return stepBuilderFactory.get("stepForGenres")
-                .<Genre, R_Genre>chunk(5)
+                .<Genre, R_Genre>chunk(CHUNK_SIZE)
                 .reader(readerGenres)
                 .processor(itemProcessorGenres)
                 .writer(writerGenres)
+                .listener(new ItemReadListener<>() {
+                    @Override
+                    public void beforeRead() {
+                        logger.info("Начало чтения");
+                    }
+
+                    @Override
+                    public void afterRead(Genre genre) {
+                        logger.info("Конец чтения");
+                    }
+
+                    @Override
+                    public void onReadError(Exception e) {
+                        logger.info("Ошибка чтения");
+                    }
+                })
+                .listener(new ItemWriteListener<>() {
+                    @Override
+                    public void beforeWrite(List list) {
+                        logger.info("Начало записи");
+                    }
+
+                    @Override
+                    public void afterWrite(List list) {
+                        logger.info("Конец записи");
+                    }
+
+                    @Override
+                    public void onWriteError(Exception e, List list) {
+                        logger.info("Ошибка записи");
+                    }
+                })
+                .listener(new ItemProcessListener<>() {
+                    @Override
+                    public void beforeProcess(Genre genre) {
+                        logger.info("Начало обработки");
+                    }
+
+                    @Override
+                    public void afterProcess(Genre genre, R_Genre r_genre) {
+                        logger.info("Конец обработки");
+                    }
+
+                    @Override
+                    public void onProcessError(Genre genre, Exception e) {
+                        logger.info("Ошибка обработки");
+                    }
+                })
+                .listener(new ChunkListener() {
+                    @Override
+                    public void beforeChunk(ChunkContext context) {
+                        logger.info("Начало пачки");
+                    }
+
+                    @Override
+                    public void afterChunk(ChunkContext context) {
+                        logger.info("Конец пачки");
+                    }
+
+                    @Override
+                    public void afterChunkError(ChunkContext context) {
+                        logger.info("Ошибка пачки");
+                    }
+                })
                 .build();
     }
 
@@ -147,10 +281,74 @@ public class JobConfig {
     public Step stepForBooks(MongoItemReader<Book> readerBooks, JpaItemWriter<R_Book> writerBooks,
                              ItemProcessor<Book, R_Book> itemProcessorBooks) {
         return stepBuilderFactory.get("stepForBooks")
-                .<Book, R_Book>chunk(5)
+                .<Book, R_Book>chunk(CHUNK_SIZE)
                 .reader(readerBooks)
                 .processor(itemProcessorBooks)
                 .writer(writerBooks)
+                .listener(new ItemReadListener<>() {
+                    @Override
+                    public void beforeRead() {
+                        logger.info("Начало чтения");
+                    }
+
+                    @Override
+                    public void afterRead(Book book) {
+                        logger.info("Конец чтения");
+                    }
+
+                    @Override
+                    public void onReadError(Exception e) {
+                        logger.info("Ошибка чтения");
+                    }
+                })
+                .listener(new ItemWriteListener<>() {
+                    @Override
+                    public void beforeWrite(List list) {
+                        logger.info("Начало записи");
+                    }
+
+                    @Override
+                    public void afterWrite(List list) {
+                        logger.info("Конец записи");
+                    }
+
+                    @Override
+                    public void onWriteError(Exception e, List list) {
+                        logger.info("Ошибка записи");
+                    }
+                })
+                .listener(new ItemProcessListener<>() {
+                    @Override
+                    public void beforeProcess(Book book) {
+                        logger.info("Начало обработки");
+                    }
+
+                    @Override
+                    public void afterProcess(Book book, R_Book r_book) {
+                        logger.info("Конец обработки");
+                    }
+
+                    @Override
+                    public void onProcessError(Book book, Exception e) {
+                        logger.info("Ошибка обработки");
+                    }
+                })
+                .listener(new ChunkListener() {
+                    @Override
+                    public void beforeChunk(ChunkContext context) {
+                        logger.info("Начало пачки");
+                    }
+
+                    @Override
+                    public void afterChunk(ChunkContext context) {
+                        logger.info("Конец пачки");
+                    }
+
+                    @Override
+                    public void afterChunkError(ChunkContext context) {
+                        logger.info("Ошибка пачки");
+                    }
+                })
                 .build();
     }
 
@@ -161,6 +359,17 @@ public class JobConfig {
                 .start(stepForAuthors)
                 .next(stepForGenres)
                 .next(stepForBooks)
+                .listener(new JobExecutionListener() {
+                    @Override
+                    public void beforeJob(JobExecution jobExecution) {
+                        logger.info("Начало job");
+                    }
+
+                    @Override
+                    public void afterJob(JobExecution jobExecution) {
+                        logger.info("Конец job");
+                    }
+                })
                 .build();
     }
 }
