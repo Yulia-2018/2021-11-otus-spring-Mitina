1d41f40ac9c5f502bd04035734f5d4638a6d77bc
MitinaUV <yulmit@mail.ru>
2022-05-14 13:13:21 +0700

    Project Add messages in TaskBoot

--- project/src/main/java/ru/otus/project/telegram/TaskBot.java
+++ project/src/main/java/ru/otus/project/telegram/TaskBot.java
@@ -1,8 +1,10 @@
 package ru.otus.project.telegram;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.otus.project.domain.User;
 import ru.otus.project.repository.UserRepository;
 
@@ -31,14 +33,35 @@ public class TaskBot extends TelegramLongPollingBot {
         if (update.hasMessage()) {
             Message message = update.getMessage();
             String userName = message.getFrom().getUserName();
-            if (!userName.isEmpty()) {
-                Optional<User> optionalUser = userRepository.getByTelegramUsername(userName);
-                if (optionalUser.isPresent()) {
-                    Long chatId = message.getChatId();
-                    User user = optionalUser.get();
-                    user.setTelegramChatId(chatId);
-                    userRepository.save(user);
+            Long chatId = message.getChatId();
+            try {
+                if (userName != null) {
+                    Optional<User> optionalUser = userRepository.getByTelegramUsername(userName);
+                    if (optionalUser.isPresent()) {
+                        User user = optionalUser.get();
+                        user.setTelegramChatId(chatId);
+                        userRepository.save(user);
+                        execute(SendMessage.builder()
+                                .chatId(chatId.toString())
+                                .text(userName + " welcome to 'Personal task management system'. Now you every day will receive a list of your tasks for today in telegram.")
+                                .build());
+                    } else {
+                        execute(SendMessage.builder()
+                                .chatId(chatId.toString())
+                                .text("Hello " + userName + "." +
+                                        " We didn't find you in 'Personal task management system'." +
+                                        " Either you haven't registered yet, or you at registration filled in field 'Telegram username' incorrectly. Please check it out.")
+                                .build());
+                    }
+                } else {
+                    execute(SendMessage.builder()
+                            .chatId(chatId.toString())
+                            .text("Hello. Your userName in telegram is empty. Please fill it out.")
+                            .build());
+
                 }
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
             }
         }
     }
